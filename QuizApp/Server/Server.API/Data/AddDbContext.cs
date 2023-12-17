a2a62880aef3fa7823580afcad7bc66a438ad096
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2023-12-17 13:45:38 +0200

    clinet and server startup

---
+++ QuizApp/Server/Server.API/Data/AddDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.API.Data.Entities;
+using System.Collections.Generic;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Quiz> Quizes { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Answer> Answers { get; set; }
+
+}
