3859b3aec027a6ef4596ad271349f9afd80ea7ff
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2024-02-22 19:12:15 +0200

    profile page added (#9)
    * client and server setup
    
    * clinet and server startup
    
    * navbar and layout adds
    
    * centered navbar elements(done for now)
    
    * home page adjustments
    
    * conflict fixed
    
    * login and register created but fr
    
    * dark and light theme changes
    
    * profile page created

---
+++ QuizApp/Client/quiz-app/app/profile/page.js
@@ -0,0 +1,120 @@
+'use client';
+import { useState } from 'react';
+
+const Page = () => {
+  const [editing, setEditing] = useState(false); // State to track whether the profile is being edited
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your update profile logic here
+    console.log('Profile updated:', formData);
+    setEditing(false); // Close the editing mode after submitting the form
+  };
+
+  return (
+    <div className="block h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center h-4/5 w-1/2 p-6 bg-white rounded-md shadow-md">
+        <section className="flex justify-center place-self-start">
+          <img
+            className="max-w-44 max-h-44 left-full mb-16 ml-10 rounded-full"
+            alt="Profile pic"
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          />
+          <button className="btn justify-center ml-16 mt-16 text-gray-100 bg-blue-700">
+            Change Image
+          </button>
+        </section>
+        {editing ? (
+          <form className="flex flex-col w-full justify-center" onSubmit={handleSubmit}>
+            <div >
+            <h2 className="text-black">Username:</h2>
+            <input
+              label="Username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              className="input w-full border-blue-700 text-black mb-4"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <h2 className="text-black">Email:</h2>
+            <input
+              label="Email"
+              type="text"
+              name="email"
+              placeholder="Email"
+              className="input w-full border-blue-700 text-black mb-4"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <h2 className="text-black">Password:</h2>
+            <input
+              label="Password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="input w-1/3 border-blue-700 text-black mb-6"
+            />
+            <a className='btn ml-10'>Change password</a>
+            </div>
+            <button type="submit" className="btn place-self-end text-gray-100 bg-blue-700">
+              Save Changes
+            </button>
+
+          </form>
+        ) : (
+            <div className='flex flex-col w-full'>
+          <button
+            onClick={() => setEditing(true)}
+            className="btn place-self-end text-gray-100 bg-blue-700"
+          >
+            Edit Profile
+          </button>
+
+          <form className='flex flex-col w-full justify-center'>
+            <h2 className='text-black'>Username:</h2>
+            <input
+              label="Username"
+              type="text"
+              name="username"
+              placeholder='Username'
+              className="input w-full border-blue-700 text-black mb-4"
+              readOnly
+            />
+            <h2 className='text-black'>Email:</h2>
+             <input
+              label="Email"
+              type="text"
+              name="Email"
+              placeholder='Email'
+              className="input w-full border-blue-700 text-black mb-4"
+              readOnly
+            />
+            <h2 className='text-black'>Password:</h2>
+            <input
+              label="Password"
+              type="password"
+              name="password"
+              placeholder='Password'
+              className="input w-full border-blue-700 text-black mb-6"
+              readOnly
+            />
+          </form>
+        </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
