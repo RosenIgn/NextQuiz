a807129e4cd9e2f7502e6b50b89423954a432178
ChaosKidd <marina.i.kuleva@gmail.com>
2024-03-09 12:11:04 +0200

    quiz craiting

--- QuizApp/Server/QuizApp.Data/Entities/Answer.cs
+++ QuizApp/Server/QuizApp.Data/Entities/Answer.cs
@@ -1,37 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Server.API.Data.Entities
+namespace QuizApp.Data.Entities
 {
     public class Answer
     {
-        public Answer(string text, bool right,string QuestionId)
-        {
-            Text=text;
-
-        }
-
-        public Answer()
-        {
-            
-        }
-
-        [Key]
-        [Required]
-        public string Id { get; init; } = Guid.NewGuid().ToString();
-
         
-        public string Text { get; set; }
-
-        [Required]
-        public bool Right { get; set; } 
-
-        [Required]
-        public string QuestionId { get; set; } 
-
-
     }
 }
--- QuizApp/Server/QuizApp.Domain/services/CreateQuizServace.cs
+++ QuizApp/Server/QuizApp.Domain/services/CreateQuizServace.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace QuizApp.Data.Entities
+namespace QuizApp.Domain.services
 {
-    public class Question
+    public class CreateQuizServace
     {
         
     }
