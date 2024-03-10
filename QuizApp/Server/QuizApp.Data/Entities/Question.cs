1393132cba12c5b1ec789e92095b44297b894393
ChaosKidd <marina.i.kuleva@gmail.com>
2024-03-10 08:56:37 +0200

    stuff from yestarday that should've been commited

--- QuizApp/Server/QuizApp.Data/Entities/Question.cs
+++ QuizApp/Server/QuizApp.Data/Entities/Question.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,22 @@ namespace QuizApp.Data.Entities
 {
     public class Question
     {
+        public Question()
+        {
+            
+        }
+
+        [Key]
+        [Required]
+        public string Id { get; init; } = Guid.NewGuid().ToString();
+
         
+        public string Text { get; set; } 
+
+        
+        public List<Answer> Answers { get; set; } = new List<Answer>();
+
+        [Required]
+        public string QuizId { get; set; }
     }
 }
--- QuizApp/Server/QuizApp.Domain/services/CreateQuizServace.cs
+++ QuizApp/Server/QuizApp.Domain/services/CreateQuizServace.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
 using System.Threading.Tasks;
+using QuizApp.Data.Entities;
+using QuizApp.Common.Requests;
+{
+    
+}
 
 namespace QuizApp.Domain.services
 {
     public class CreateQuizServace
     {
-        
+        public Answer CreateAnswer(CreateAnswerRequest answerRequest)
+        {
+            
+            return ;
+        }
     }
 }
