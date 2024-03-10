f378f5e8311586fa00225e28ebf3280ddb513985
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2024-03-10 19:13:22 +0200

    login page fixed

--- QuizApp/Client/quiz-app/app/login/page.js
+++ QuizApp/Client/quiz-app/app/login/page.js
@@ -34,12 +34,33 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center max-w-md w-full p-6 bg-white rounded-md shadow-md">
-        <h2 className="text-3xl text-main-blue font-extrabold mb-6">Login</h2>
-        <LoginForm onSubmit={handleSubmit} />
+    <div className="min-h-screen bg-light-blue flex items-center justify-center">
+        <div className="flex flex-col items-center justify-center max-w-md w-full p-6 bg-base-100 rounded-lg shadow-md">
+          <h2 className="text-3xl text-main-blue font-extrabold mb-6">Login</h2>
+          <form className='flex flex-col w-full items-center justify-center' onSubmit={handleSubmit}>
+            <input
+              label="Username"
+              type="text"
+              name="username"
+              placeholder='Username'
+              value={formData.username}
+              onChange={handleChange}
+              className="input w-full border-light-blue text-black mb-4"
+            />
+            <input
+              label="Password"
+              type="password"
+              name="password"
+              placeholder='Password'
+              value={formData.password}
+              onChange={handleChange}
+              className="input w-full border-light-blue text-black mb-6"
+            />
+            <br/>
+            <button className='btn w-full hover:text-base-content bg-main-blue text-base-200' type="submit">Login</button>
+          </form>
+        </div>
       </div>
-    </div>
   );
 };
 
