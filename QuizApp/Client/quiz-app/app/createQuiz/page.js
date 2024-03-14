8d16f5fe9bec774d0735e52a82d0f1b78063f0cf
NikolaGrigorov0 <129086579+NikolaGrigorov0@users.noreply.github.com>
2024-03-14 20:02:28 +0200

    complete button on create quiz

--- QuizApp/Client/quiz-app/app/createQuiz/page.js
+++ QuizApp/Client/quiz-app/app/createQuiz/page.js
@@ -5,6 +5,10 @@ import { useState } from "react";
 const Page = () => {
   const [questions, setQuestions] = useState([{ question: "", options: [""] }]);
 
+  const submitHandler = () => {
+    const qeustionObj = questions;
+  }
+
   const addQuestion = () => {
     setQuestions([...questions, { question: "", options: [""] }]);
   };
@@ -40,7 +44,7 @@ const Page = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="flex flex-col container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Create Quiz</h1>
 
       {questions.map((question, questionIndex) => (
@@ -48,27 +52,28 @@ const Page = () => {
           key={questionIndex}
           className="flex flex-row justify-center bg-base-300 space-y-4 rounded-md w-full mb-4"
         >
-        <setion id="question field" className='flex flex-col mr-5'>
-
-          <div
-            className="badge mr-10 mt-10 w-1/2 self-center"
-            label={`Question ${questionIndex + 1}`}
-            onChange={(e) =>
+          <setion id="question field" className="flex flex-col mr-5">
+            <div
+              className="badge mr-10 mt-10 w-1/2 self-center"
+              label={`Question ${questionIndex + 1}`}
+              onChange={(e) =>
                 handleQuestionChange(questionIndex, e.target.value)
-            }
+              }
             >
-            {`Question ${questionIndex + 1}`}
-          </div>
+              {`Question ${questionIndex + 1}`}
+            </div>
 
-          <textarea className="textarea flex-row text-md w-1/2 max-h-44 mt-10 m-20">
-          </textarea>
-
-        </setion>
+            <textarea className="textarea flex-row text-md w-1/2 max-h-44 mt-10 m-20"></textarea>
+          </setion>
 
           <div className="space-y-2">
             {question.options.map((option, optionIndex) => (
               <div key={optionIndex} className="flex items-center space-x-2">
-                <input type="radio" name={`radio-${questionIndex + 1}`} className="radio mb-5" />
+                <input
+                  type="radio"
+                  name={`radio-${questionIndex + 1}`}
+                  className="radio mb-5"
+                />
                 <input
                   className="input border-light-blue mb-5 shadow-md"
                   onChange={(e) =>
@@ -88,9 +93,11 @@ const Page = () => {
                 </button>
               </div>
             ))}
-            {question.options.length <= 3 && (<button className="btn" onClick={() => addOption(questionIndex)}>
-              Add Option
-            </button>)}
+            {question.options.length <= 3 && (
+              <button className="btn" onClick={() => addOption(questionIndex)}>
+                Add Option
+              </button>
+            )}
           </div>
 
           <div
@@ -105,6 +112,7 @@ const Page = () => {
       <button className="btn" onClick={addQuestion}>
         Add Question
       </button>
+      <button className="btn mt-5 text-base-100 bg-success" onClick={submitHandler()}>Complete</button>
     </div>
   );
 };
