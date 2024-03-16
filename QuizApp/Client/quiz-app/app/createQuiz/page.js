89ec3ae21ff82feee04f1ad92b481da88fcb380b
b-demiran <b.demiran06@gmail.com>
2024-03-16 10:13:14 +0200

    create quiz page remove quiz button improvement

--- QuizApp/Client/quiz-app/app/createQuiz/page.js
+++ QuizApp/Client/quiz-app/app/createQuiz/page.js
@@ -6,7 +6,7 @@ const Page = () => {
   const [questions, setQuestions] = useState([{ question: "", options: [""] }]);
 
   const submitHandler = () => {
-    const qeustionObj = questions;
+    // Handle submit logic here
   }
 
   const addQuestion = () => {
@@ -48,71 +48,71 @@ const Page = () => {
       <h1 className="text-3xl font-bold mb-4">Create Quiz</h1>
 
       {questions.map((question, questionIndex) => (
-        <div
-          key={questionIndex}
-          className="flex flex-row justify-center bg-base-300 space-y-4 rounded-md w-full mb-4"
-        >
-          <setion id="question field" className="flex flex-col mr-5">
-            <div
-              className="badge mr-10 mt-10 w-1/2 self-center"
-              label={`Question ${questionIndex + 1}`}
-              onChange={(e) =>
-                handleQuestionChange(questionIndex, e.target.value)
-              }
-            >
-              {`Question ${questionIndex + 1}`}
+        <div key={questionIndex} className="relative mb-4">
+          <div className="flex flex-row justify-center bg-base-300 space-y-4 rounded-md w-full">
+            <div className="flex flex-col mr-5">
+              <div
+                className="badge mr-10 mt-10 w-1/2 self-center"
+                label={`Question ${questionIndex + 1}`}
+                onChange={(e) =>
+                  handleQuestionChange(questionIndex, e.target.value)
+                }
+              >
+                {`Question ${questionIndex + 1}`}
+              </div>
+
+              <textarea className="textarea flex-row text-md w-1/2 max-h-44 mt-10 m-20"></textarea>
             </div>
 
-            <textarea className="textarea flex-row text-md w-1/2 max-h-44 mt-10 m-20"></textarea>
-          </setion>
-
-          <div className="space-y-2">
-            {question.options.map((option, optionIndex) => (
-              <div key={optionIndex} className="flex items-center space-x-2">
-                <input
-                  type="radio"
-                  name={`radio-${questionIndex + 1}`}
-                  className="radio mb-5"
-                />
-                <input
-                  className="input border-light-blue mb-5 shadow-md"
-                  onChange={(e) =>
-                    handleOptionChange(
-                      questionIndex,
-                      optionIndex,
-                      e.target.value
-                    )
-                  }
-                />
-                <button
-                  className="btn mb-5 text-xl text-wrong-red"
-                  onClick={() => removeOption(questionIndex, optionIndex)}
-                >
-                  {" "}
-                  X{" "}
+            <div className="space-y-2">
+              {question.options.map((option, optionIndex) => (
+                <div key={optionIndex} className="flex items-center space-x-2">
+                  <input
+                    type="radio"
+                    name={`radio-${questionIndex + 1}`}
+                    className="radio mb-5"
+                  />
+                  <input
+                    className="input border-light-blue mb-5 shadow-md"
+                    onChange={(e) =>
+                      handleOptionChange(
+                        questionIndex,
+                        optionIndex,
+                        e.target.value
+                      )
+                    }
+                  />
+                  <button
+                    className="btn mb-5 text-xl text-wrong-red"
+                    onClick={() => removeOption(questionIndex, optionIndex)}
+                  >
+                    {" "}
+                    X{" "}
+                  </button>
+                </div>
+              ))}
+              {question.options.length <= 3 && (
+                <button className="btn" onClick={() => addOption(questionIndex)}>
+                  Add Option
                 </button>
-              </div>
-            ))}
-            {question.options.length <= 3 && (
-              <button className="btn" onClick={() => addOption(questionIndex)}>
-                Add Option
-              </button>
-            )}
+              )}
+            </div>
           </div>
-
-          <div
-            className="btn ml-10 mb-5 self-end"
+          <button
+            className="btn absolute bg-transparent border-none shadow-none text-lg top-0 right-0 mt-2 mr-2"
             onClick={() => removeQuestion(questionIndex)}
           >
-            Remove Question
-          </div>
+            X
+          </button>
         </div>
       ))}
 
       <button className="btn" onClick={addQuestion}>
         Add Question
       </button>
-      <button className="btn mt-5 text-base-100 bg-success" onClick={submitHandler()}>Complete</button>
+      <button className="btn mt-5 text-base-100 bg-success" onClick={submitHandler}>
+        Complete
+      </button>
     </div>
   );
 };
