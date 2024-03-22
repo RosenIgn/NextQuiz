cc70e2f106f97b13178f88d55ce0d76e181c2965
RosenIgn <rosen.ign@gmail.com>
2024-03-22 19:08:56 +0200

    quizzes back end and front end, footer page color change

--- QuizApp/Client/quiz-app/app/joinQuiz/[id]/page.js
+++ QuizApp/Client/quiz-app/app/joinQuiz/[id]/page.js
@@ -1,139 +1,188 @@
-'use client';
+"use client";
 
-import { getURL } from 'next/dist/shared/lib/utils';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const PopUp = ({ message, onClose }) => (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-8 rounded-md">
-        <p className="text-lg">{message}</p>
-        <button className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4" onClick={onClose}>
-          Close
-        </button>
-      </div>
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+    <div className="bg-white p-8 rounded-md">
+      <p className="text-lg">{message}</p>
+      <button
+        className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4"
+        onClick={onClose}
+      >
+        Close
+      </button>
     </div>
-  );
-  
-  const Page = () => {
-    const [quiz, setQuiz] = useState([
-      {
-        id: 1,
-        question: 'Question 1?',
-        options: ['Option 1', 'Option 2', 'Option 3'],
-        selectedOption: '',
-        correctOption: 'Option 2',
-        points: 1,
-      },
-      {
-        id: 2,
-        question: 'Question 2?',
-        options: ['Option 1', 'Option 2', 'Option 3'],
-        selectedOption: '',
-        correctOption: 'Option 3',
-        points: 1,
-      },
-    ]);
-  
-    const [results, setResults] = useState(null);
-    const [submitPressed, setSubmitPressed] = useState(false);
-    const [popUpMessage, setPopUpMessage] = useState('');
-  
-    const handleOptionChange = (questionId, selectedOption) => {
-      setQuiz((prevQuiz) =>
-        prevQuiz.map((question) =>
-          question.id === questionId ? { ...question, selectedOption } : question
-        )
-      );
-    };
-  
-    const handleSubmit = () => {
-      const isAllAnswered = quiz.every((question) => question.selectedOption !== '');
-  
-      if (!isAllAnswered) {
-        setPopUpMessage('Please answer all questions.');
-        return;
+  </div>
+);
+
+const Page = () => {
+  const params = useParams();
+
+  const [quiz, setQuiz] = useState([]);
+  const [time, setTime] = useState(120 - 1);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
+  const [results, setResults] = useState(null);
+  const [submitPressed, setSubmitPressed] = useState(false);
+  const [popUpMessage, setPopUpMessage] = useState("");
+
+  useEffect(() => {
+    const getQuizData = async () => {
+      try {
+        const response = await fetch(
+          `https://localhost:5074/api/Quiz/GetQuizData/${String(params.id)}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        const data = await response.json();
+
+        const quizData = data.map((question) => ({
+          ...question,
+          selectedOption: "",
+        }));
+
+        setQuiz(quizData);
+      } catch (error) {
+        console.error("Error fetching user:", error);
       }
-  
-      const totalPoints = quiz.reduce((acc, question) => {
-        const isCorrect = question.selectedOption === question.correctOption;
-        return isCorrect ? acc + question.points : acc;
-      }, 0);
-  
-      setResults({
-        totalPoints,
-        correctAnswers: totalPoints,
-        incorrectAnswers: quiz.length - totalPoints,
-      });
-  
-      setSubmitPressed(true);
     };
-  
-    const handleTryAgain = () => {
-      window.location.reload();
-    };
-  
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="container mx-auto p-4">
-          <h1 className="text-3xl font-bold mb-4">Current Quiz</h1>
-          {quiz.map((part) => (
-            <div
-              key={part.id}
-              className={`bg-white rounded-lg shadow-md p-6 mb-4 ${
-                submitPressed &&
-                (part.selectedOption === part.correctOption ? 'bg-green-100' : 'bg-red-100')
-              }`}
-            >
-              <h2 className="text-xl font-semibold mb-2">{part.question}</h2>
-              <div className="space-y-2">
-                {part.options.map((option, index) => (
-                  <div key={index} className="flex items-center">
-                    <input
-                      type="radio"
-                      id={`option-${index}`}
-                      name={`question-${part.id}`}
-                      value={option}
-                      className="radio-md mr-2"
-                      checked={part.selectedOption === option}
-                      onChange={() => handleOptionChange(part.id, option)}
-                      disabled={submitPressed}
-                    />
-                    <label htmlFor={`option-${index}`} className="text-lg text-gray-700">
-                      {option}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-          <button
-            className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4"
-            onClick={handleSubmit}
-            disabled={submitPressed}
-          >
-            Submit
-          </button>
-          {submitPressed && (
-            <button
-              className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4 ml-2"
-              onClick={handleTryAgain}
-            >
-              Try Again
-            </button>
-          )}
-          {results && (
-            <div className="mt-4">
-              <h2 className="text-2xl font-semibold mb-2">Results</h2>
-              <p>Total Questions: {quiz.length}</p>
-              <p>Correct Answers: {results.correctAnswers}</p>
-              <p>Incorrect Answers: {results.incorrectAnswers}</p>
-            </div>
-          )}
-        </div>
-        {popUpMessage && <PopUp message={popUpMessage} onClose={() => setPopUpMessage('')} />}
-      </div>
+
+    getQuizData();
+  }, []);
+
+  useEffect(() => {
+    if (isTimerRunning) {
+      const timer = setTimeout(() => {
+        setTime((prevValue) => (prevValue <= 0 ? 0 : prevValue - 1));
+      }, 1000);
+
+      if (time === 0) {
+        finishQuiz(true);
+      }
+
+      return () => clearTimeout(timer);
+    }
+  }, [isTimerRunning, time]);
+
+  const handleOptionChange = (questionId, selectedOption) => {
+    setQuiz((prevQuiz) =>
+      prevQuiz?.map((question) =>
+        question.id === questionId ? { ...question, selectedOption } : question
+      )
     );
   };
-  
-  export default Page;
+
+  const finishQuiz = (isTimeExpired) => {
+    if (!isTimeExpired) {
+      const isAllAnswered = quiz.every(
+        (question) => question.selectedOption !== ""
+      );
+
+      if (!isAllAnswered) {
+        setPopUpMessage("Please answer to all of the questions.");
+        return;
+      }
+    }
+
+    const totalPoints = quiz.reduce((acc, question) => {
+      const isCorrect = question.selectedOption === question.correctAnswer;
+      return isCorrect ? acc + 1 : acc;
+    }, 0);
+
+    setResults({
+      totalPoints,
+      correctAnswers: totalPoints,
+      incorrectAnswers: quiz.length - totalPoints,
+    });
+
+    setSubmitPressed(true);
+    setIsTimerRunning(false);
+  };
+
+  const handleTryAgain = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-light-blue">
+      <div className="container mx-auto p-4 text-gray-900">
+        <h1 className="text-3xl mb-2">
+          Time Left: {Math.floor((time % 3600) / 60)}:
+          {String(time % 60).padStart(2, "0")}
+        </h1>
+        {quiz?.map((part, index) => (
+          <div
+            key={index + 1}
+            className={`rounded-lg shadow-md p-6 mb-4 ${
+              submitPressed
+                ? part.selectedOption === part.correctAnswer
+                  ? "bg-green-100"
+                  : "bg-red-100"
+                : "bg-white"
+            }`}
+          >
+            <h2 className="text-xl font-semibold mb-2">
+              Question №{index + 1}: {part.label}
+            </h2>
+            <div className="space-y-2">
+              {part.answers.map((option, index) => (
+                <div key={index} className="flex items-center">
+                  <input
+                    type="radio"
+                    id={`option-${index}`}
+                    name={`question-${part.id}`}
+                    value={option}
+                    className="radio-md mr-2"
+                    checked={part.selectedOption === option}
+                    onChange={() => handleOptionChange(part.id, option)}
+                    disabled={submitPressed}
+                  />
+                  <label
+                    htmlFor={`option-${index}`}
+                    className="text-lg text-gray-700"
+                  >
+                    {option}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <button
+          className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4"
+          onClick={() => finishQuiz(false)}
+          disabled={submitPressed}
+        >
+          Submit
+        </button>
+        {submitPressed && (
+          <button
+            className="btn bg-blue-500 text-white py-2 px-4 rounded-md mt-4 ml-2"
+            onClick={handleTryAgain}
+          >
+            Try Again
+          </button>
+        )}
+        {results && (
+          <div className="mt-4">
+            <h2 className="text-2xl font-semibold mb-2">Results</h2>
+            <p>Total Questions: {quiz.length}</p>
+            <p>Correct Answers: {results.correctAnswers}</p>
+            <p>Incorrect Answers: {results.incorrectAnswers}</p>
+          </div>
+        )}
+      </div>
+      {popUpMessage && (
+        <PopUp message={popUpMessage} onClose={() => setPopUpMessage("")} />
+      )}
+    </div>
+  );
+};
+
+export default Page;
